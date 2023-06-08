1e31f55ad09ea031b4706e177ef78511296b72f8
Dev_Yoshi <fujitaniyoshilynp@gmail.com>
2023-06-08 21:16:45 +0800

    add: Sidebar for Mobile and Navbar for Desktop

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 const Header = () => {
     const gradienttext = `inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-4xl text-transparent font-semibold`
   return (
-    <motion.div className='pt-40 px-6 text-white overflow-clip'  initial={{ y: -100 }} // Initial position off-screen
+    <motion.div className='container pt-40 px-6 text-white overflow-clip lg:pl-[400px] lg:pr-48'  initial={{ y: -100 }} // Initial position off-screen
     animate={{ y: 0 }} // Final position on-screen
     transition={{ type: 'spring', duration: 1 }}>
      
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const Navbar = () => {
+
+  const [open, setmenu] = useState(true)
+  const [isRendered, setIsRendered] = useState(false);
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
   return (
-    <div className='bg-Main py-6 px-6 shadow-xl fixed top-0 w-full'>
-         <div className="container flex justify-between items-center text-white lg:hidden">
+    <>
+    <AnimatePresence>
+      {open ? (<div className='bg-Main py-6 px-6 shadow-xl fixed top-0 w-full lg:hidden'
+       initial={{ x: -300 }} // Initial position off-screen
+        animate={{ x: 0 }} // Move back to off-screen when open is false
+        transition={{ type: 'spring', duration: 0.5}}>
+        <div className="container flex justify-between items-center text-white lg:hidden">
           {/* Logo and Name */}
           <div className="flex items-center space-x-2">
-                <h1 className='w-9 h-9 bg-white rounded-full'></h1>
-                <div className="leading-tight">
-                    <h1 className='inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-xl text-transparent'>Yoshilyn Fujitani</h1>
-                    <p className='text-subtext  text-sm  ' >Web Developer</p>
-                </div>
+            <h1 className='w-9 h-9 bg-white rounded-full'></h1>
+            <div className="leading-tight">
+              <h1 className='inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-md text-transparent'>Yoshilyn Fujitani</h1>
+              <p className='text-subtext  text-sm  ' >Web Developer</p>
+            </div>
           </div>
           {/* Menu */}
           <div className="">
-              Menu
+            <button onClick={() => setmenu(!open)}>Menu</button>
           </div>
         </div>
-    </div>
+      </div>) : (
+        <motion.div className='block z-10'
+        key="sidebar"
+        initial={{ x: -300 }} // Initial position off-screen
+        animate={{ x: 0 }} // Move back to off-screen when open is false
+        transition={{ type: 'spring', duration: 0.25}}
+        exit={{ x: -300 }}  >
+        <div className="bg-Main w-64 h-screen fixed top-0 flex flex-col  items-center">
+        <button onClick={() => setmenu(!open)}>Close</button>
+          {/* Logo and Name */}
+        <div className="flex items-center space-x-2 py-6">
+              <h1 className='w-9 h-9 bg-white rounded-full'></h1>
+              <div className="leading-none">
+                <h1 className=' animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-lg text-transparent'>Yoshilyn Fujitani</h1>
+                <p className='text-subtext  text-sm  ' >Web Developer</p>
+              </div>
+              </div>
+        </div>
+  
+       </motion.div>
+     
+      )}
+      </AnimatePresence>
+      {/* Desktop */}
+     <div className="hidden lg:block">
+      <div className="bg-Main w-64 h-screen fixed top-0 flex flex-col  items-center">
+        {/* Logo and Name */}
+      <div className="flex items-center space-x-2 py-6">
+            <h1 className='w-9 h-9 bg-white rounded-full'></h1>
+            <div className="leading-none">
+              <h1 className=' animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-lg text-transparent'>Yoshilyn Fujitani</h1>
+              <p className='text-subtext  text-sm  ' >Web Developer</p>
+            </div>
+            </div>
+      </div>
+
+     </div>
+      
+      
+
+    </>
   )
 }
 
