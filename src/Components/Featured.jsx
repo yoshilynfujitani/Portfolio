19393de8207656f985dd0b30ff206e8eef893dd4
Dev_Yoshi <fujitaniyoshilynp@gmail.com>
2023-06-09 13:59:05 +0800

    Edit: asset file paths

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -6,7 +6,7 @@ const Featured = () => {
     <div className="container px-6">
       {/* card */}
       <div className="pr-5 py-4 cursor-pointer">
-      <img src="/public/Habiya.png" alt="" className='rounded-md transition hover:scale-105 '/>
+      <img src="../src/assets/Habiya.png" alt="" className='rounded-md transition hover:scale-105 '/>
       <p className='text-white text-xl font-bold py-2'>Habiya: <span className='font-thin'> An Ilocano E-Commerce Project</span></p>
       <p className='text-subtext'>for CMPSC 117</p>
       <p><AiFillGithub className='text-4xl text-subtext hover:scale-105'/></p>
