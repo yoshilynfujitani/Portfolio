cde56ad2b09298720748451797f0fda7fda8ef6d
Dev_Yoshi <fujitaniyoshilynp@gmail.com>
2023-06-10 17:50:39 +0800

    Add: Made Feature reusable component, icons, images

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { AiFillGithub } from "react-icons/ai";
-import Habiya from "/Habiya.png"
 
-const Featured = () => {
+
+const Featured = (props) => {
   return (
     <div className="container px-6  lg:px-0">
       {/* card */}
-      <div className="pr-5 py-4 cursor-pointer">
-      <img src={Habiya} alt="" className='rounded-md transition hover:scale-105 '/>
-      <p className='text-white text-xl font-bold py-2'>Habiya: <span className='font-thin'> An Ilocano E-Commerce Project</span></p>
-      <p className='text-subtext'>for CMPSC 117</p>
-      <p><AiFillGithub className='text-4xl text-subtext hover:scale-105'/></p>
+      <div className="py-4 cursor-pointer">
+      <img src={props.img} alt="" className='rounded-md transition hover:scale-105 '/>
+      <p className='text-white text-xl font-bold py-2'>{props.title}<span className='font-thin'>{props.desc}</span></p>
+      <p className='text-subtext'>{props.client}</p>
+     <div className="flex space-x-2">
+      {props.stack.map((item) => {
+          const IconComponent = item;
+          return <span key={item.id} className='text-3xl lg:text-4xl text-subtext hover:scale-105'><IconComponent /></span>;
+        })}
+     </div>
       </div>
     </div>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,23 +2,62 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Header from '../Components/Header'
 import Featured from '../Components/Featured'
+import Habiya from "/Habiya.png"
+import Weather from "/Weather.png"
+import Pokedex from "/Pokedex.png"
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillHtml5 } from "react-icons/ai";
+import { DiCss3 } from "react-icons/di";
+import { DiJavascript } from "react-icons/di";
+import { DiPhp } from "react-icons/di";
+import { SiTailwindcss } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+
+
 
 const Home = () => {
+
+  const works = [
+    {
+      id:1,
+      title:"Habiya",
+      desc:"An Ilocano E-Commerce Project",
+      img: Habiya,
+      stack:[AiFillGithub,AiFillHtml5,DiCss3,SiTailwindcss,DiJavascript,DiPhp,SiMysql]
+    },
+    {
+      id:2,
+      title:"Weather App",
+      desc:"API integrated project",
+      img: Weather,
+      stack:[AiFillGithub,AiFillHtml5,DiCss3,SiTailwindcss,DiJavascript]
+    },
+    {
+      id:3,
+      title:"Pokedex",
+      desc:"API integrated project",
+      img: Pokedex,
+      stack:[AiFillGithub,AiFillHtml5,DiCss3,SiTailwindcss,DiJavascript]
+    }
+  ]
+
+
   return (
     <div className="bg-Background min-h-screen w-screen overflow-clip">
- 
-      
-      <Header  />
+
+
+      <Header />
       <div className="container lg:pl-[400px] lg:pr-48 lg:pt-20">
-      <h1 className=' inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-lg font-semibold text-transparent px-6 pt-10 lg:px-0 lg:pt-0'>Featured Works</h1>
+        <h1 className=' inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-lg font-semibold text-transparent px-6 pt-10 lg:px-0 lg:pt-0'>Featured Works</h1>
       </div>
       <div className="container grid grid-cols-1 lg:grid-cols-2 lg:pl-[400px]">
-      <Featured/>
-      <Featured/>
+        {works.map((item) => (
+          <Featured {...item}  key={item.id}/>
+        ))}
       </div>
-      
-  
-      </div>
+
+
+    </div>
   )
 }
 
