8d9e982f61eb27e111ee7d6393c55b519a53e213
yoshi.dev <fujitaniyoshilynp@gmail.com>
2023-08-19 08:56:46 +0800

    Removed smooth-scroll dependency

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -11,11 +11,11 @@ const Featured = (props) => {
             <span className="font-thin pl-2">{props.desc}</span>
           </p>
           <p className="text-gray-500 dark:text-subtext">{props.client}</p>
-          <div className="flex space-x-2">
+          <div className="my-1 grid grid-cols-7 md:grid-cols-10 gap-2">
             {props.stack.map((item, index) => {
               const IconComponent = item;
 
-              const iconClass = `transition text-3xl lg:text-4xl  text-gray-700 dark:text-subtext hover:scale-105 hover:text-slate-50`;
+              const iconClass = `transition text-3xl lg:text-4xl  text-gray-700 dark:text-subtext hover:scale-105 hover:text-slate-900 dark:hover:text-slate-50 `;
 
               return (
                 <span key={index} className={iconClass}>
@@ -27,12 +27,12 @@ const Featured = (props) => {
           <p className="text-md font-thin dark:text-subtext">{props.purpose}</p>
         </div>
       </div>
-      <div className="text-center pb-4">
+      <div className="text-center m-2 bg-gray-200 rounded-md dark:bg-Background">
         <a
           href={props.link}
           target="blank"
           rel="noopener noreferrer"
-          className="text-gray-900 dark:text-gray-200 transition-colors text-md md:text-lg text-transparent cursor-pointer flex items-center justify-center gap-1 hover:text-purple-100"
+          className="text-gray-800 dark:text-gray-200 transition-colors text-md md:text-lg text-transparent cursor-pointer flex items-center justify-center gap-1 hover:text-slate-800 dark:hover:text-purple-100"
         >
           <span>
             <AiFillGithub />
