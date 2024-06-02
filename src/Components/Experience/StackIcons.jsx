cb3a2c180c9608a18ad413710d7d918d7c34de58
Yoshi.dev <fujitaniyoshilynp@gmail.com>
2024-06-02 19:05:26 +0800

    minor ui fix

--- src/Components/Experience/StackIcons.jsx
+++ src/Components/Experience/StackIcons.jsx
@@ -4,17 +4,15 @@ import Container from "../../Layout/Container";
 const StackIcons = ({ props }) => {
   const { image, title, subtitle } = props;
   return (
-    <Container>
-      <div className="transition-all flex items-center space-x-4 rounded-md py-2 px-4 hover:bg-lightModeSecondary dark:hover:bg-Main hover:cursor-pointer">
-        <div className="">
-          <img src={image} alt="" className="w-10 h-10 object-contain" />
-        </div>
-        <div className="flex flex-col ">
-          <h1 className="text-main font-semibold">{title}</h1>
-          <h1 className="text-subtext">{subtitle}</h1>
-        </div>
+    <div className="transition-all flex items-center space-x-4 rounded-md py-2 px-4 hover:bg-lightModeSecondary dark:hover:bg-Main hover:cursor-pointer">
+      <div className="">
+        <img src={image} alt="" className="w-10 h-10 object-contain" />
       </div>
-    </Container>
+      <div className="flex flex-col ">
+        <h1 className="text-main font-semibold">{title}</h1>
+        <h1 className="text-subtext">{subtitle}</h1>
+      </div>
+    </div>
   );
 };
 
